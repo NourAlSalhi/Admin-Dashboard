4517dd3af074859315980f9d331cbc81f59c85bb
Nour Al Salhi <nuraym9998@gmail.com>
2024-08-27 09:55:08 +0300

    edit layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components";
+import { Navbar, Sidebar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className="flex">
+          <div className="hidden md:block h-[100vh] w-[300px]">
+            <Sidebar />
+          </div>
+          <div className="w-full p-5 md:max-w-[1140px]">{children}</div>
+        </div>
       </body>
     </html>
   );
